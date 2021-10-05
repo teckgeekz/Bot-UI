378252452c7f23ffcd8565c8cfedf83a42f953d9
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2021-10-05 16:41:10 +0530

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,8 +3,8 @@
     <div class="row q-col-gutter-md">
       <div class="col-8">
         <div class="row items-center q-pb-md">
-          <div class="text-h1 col">Gekko</div>
-          <div class="text-h2 col">Gordon UI edition</div>
+          <div class="text-h1 col">Custom Advanced Bot</div>
+          <div class="text-h2 col">Custom BOT UI Latest edition</div>
         </div>
         <p>
           BOT UI is a Bitcoin/Altcoin trading bot and backtesting platform <br>
